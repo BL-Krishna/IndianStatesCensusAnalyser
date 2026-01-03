a8a663fc8e6aa026022c24a16e97940ee3798458
dattathreya06 <dattathreyachandana@gmail.com>
2026-01-03 15:03:39 +0530

    Welcome message added

---
+++ src/main/java/com/bridgelabz/censusanalyser/CensusAnalyserMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.censusanalyser;
+
+public class CensusAnalyserMain {
+    static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser");
+
+    }
+}
