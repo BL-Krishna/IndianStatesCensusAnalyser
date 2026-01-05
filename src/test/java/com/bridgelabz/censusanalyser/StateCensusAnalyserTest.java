a0c7ef5f68bfaf76f4ea78381046c688a2d04009
dattathreya06 <dattathreyachandana@gmail.com>
2026-01-05 14:25:22 +0530

    [Krishna] Updated StateCensusAnalyserTest

--- src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTest.java
@@ -8,7 +8,7 @@ public class StateCensusAnalyserTest {
 
     private static final String STATE_CENSUS_CSV =
             "src/main/resources/IndiaStateCensusData.csv";
-
+    //UC 1.1 - Happy Test Case
     @Test
     public void givenCorrectCSV_ShouldReturnCorrectCount()
         throws CensusAnalyserException{
@@ -16,7 +16,7 @@ public class StateCensusAnalyserTest {
             int count  = analyser.loadStateCensusData(STATE_CENSUS_CSV);
             Assertions.assertEquals(29,count);
     }
-
+    //UC 1.2 - Incorrect File(Sad test case)
     @Test
     public void givenIncorrectFile_ShouldThrowException(){
         StateCensusAnalyser analyser=new StateCensusAnalyser();
@@ -27,7 +27,7 @@ public class StateCensusAnalyserTest {
 
         );
     }
-
+    //UC 1.3 - Wrong FileType
     @Test
     public void givenWrongFile_ShouldThrowException(){
        StateCensusAnalyser analyser =new StateCensusAnalyser();
@@ -37,6 +37,32 @@ public class StateCensusAnalyserTest {
                ()-> analyser.loadStateCensusData("IndianStateCensusData.txt")
        );
     }
+    //UC 1.4 - Wrong Delimiter
+    @Test
+    public void givenWrongDelimiter_ShouldThrowException() {
+
+        StateCensusAnalyser analyser = new StateCensusAnalyser();
+
+        Assertions.assertThrows(
+                CensusAnalyserException.class,
+                () -> analyser.loadStateCensusData(
+                        "src/main/resources/wrong_delimiter.csv")
+        );
+    }
+    //UC 1.5 - Wrong Header
+    @Test
+    public void givenWrongHeader_ShouldThrowException() {
+
+        StateCensusAnalyser analyser = new StateCensusAnalyser();
+
+        Assertions.assertThrows(
+                CensusAnalyserException.class,
+                () -> analyser.loadStateCensusData(
+                        "src/main/resources/wrong_header.csv")
+        );
+    }
+
+
 
 
 
